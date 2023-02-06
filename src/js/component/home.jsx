1769d29ed4bf63369ce77ee5ecdbc4e5fbf59765
Daianamelo <melocecilia.daiana@gmail.com>
2023-02-06 01:03:18 +0000

    My first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,141 +1,120 @@
-import React, {useState, useEffect} from "react";//importo react y el hook useState
+import React, { useState } from "react";
 
 
-
-//create your first component
 const Home = () => {
+  //Inicialización del estado para el valor del input
+  const [taskInput, setTaskInput] = useState("");
+  //Inicialización del estado para la lista de tareas
+  const [tasks, setTasks] = useState([]);
+//Función para manejar los cambios en el valor del input
+  const handleTaskInput = (e) => {
+    setTaskInput(e.target.value);
+  };
+
+  //Función para agregar una nueva tarea a la lista
+  const addTask = (e) => {
+    e.preventDefault();
+    setTasks(tasks.concat(taskInput));
+    setTaskInput("");
+  };
+
+//Función para crear un usuario en la API
+const createUser = () => {
+  fetch(`https://assets.breatheco.de/apis/fake/todos/user/esposadesuga`,{
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify([])
+  })
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+};
 
 
-
-  //Haz que tu TODO List se sincronice con la API de backend cada vez que se agregue o elimine una tarea.
-
-
-
-
-	//declaro los estados//
-	const[pendientes,setPendientes]=useState("")//1. creamos un estado del input pendientes
-        //[espacio donde guardo los valores,funcion que actualiza los valores]
-	//creo funcion para ejecutar la actualizacion del valor inicial
-
-	const [data, setData] = useState([]); // 2. creamos un estado de lo que se guarda en el array
+//Función para obtener la lista de tareas desde la API
+const getTodoList = () => {
+  fetch(`https://assets.breatheco.de/apis/fake/todos/user/esposadesuga`, {
+    method: 'GET',
+  })
+  .then((response) => response.json())
+  .then((data) => setTasks(data));
+};
 
 
-	function handlePendientes (e) {
-		//console.log(e.target.value)//para poder ver el valor del target en el que esta posicionado
-		setPendientes(e.target.value) 
+//Función para actualizar la lista de tareas desde la API
+
+
+  function updateTasks(){
+		fetch(`https://assets.breatheco.de/apis/fake/todos/user/esposadesuga`,
+		{method: 'PUT', 
+		headers: {
+			'Content-Type': 'application/json'},
+		body: JSON.stringify(list)
+	  })
+		.then((response)=>response.json())
+		.then((data)=>console.log(data))
 	}
 
-
-	function enviarPendientes (e){
-		e.preventDefault()// detenemos el comportamiento predeterminado para procesar nuestro codigo
-		setData(data.concat(pendientes))
-    setPendientes("");
-
-	} 
-	console.log(pendientes);
-
-  function crearUsuario(){
-    fetch("https://assets.breatheco.de/apis/fake/todos/user/esposadesuga",{
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify([])
-    })
-    .then((response)=>response.json())
-    .then((data)=>console.log(data))
-  }
-
-  function obtenerTodoList (){
-    fetch("https://assets.breatheco.de/apis/fake/todos/user/esposadesuga",{
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json"
-      },
-    })
-    .then((response)=>response.json())
-    .then((data)=>console.log(data))
-  }
-  function update(){
-    fetch("https://assets.breatheco.de/apis/fake/todos/user/esposadesuga",{
-      method: "PUT",//ACTUALIZA
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
-    }).then((response)=>response.json())
-    .then((data)=>console.log(data))
-  }
-  console.log(update())
-
-  function eliminarTodoses() {
-    fetch("https://assets.breatheco.de/apis/fake/todos/user/esposadesuga",{
-      method: "DELETE",//ACTUALIZA
-      headers: {
-        "Content-Type": "application/json"
-      }
-  }) .then((response)=>response.json())
-  .then((data)=>{console.log(data.result)
-    if (data.result==="ok"){
-      setData([])
+//para borrar las en la api
+function deleteTasks(){
+  fetch(`https://assets.breatheco.de/apis/fake/todos/user/esposadesuga`,
+  {method: 'DELETE', 
+  headers: {
+    'Content-Type': 'application/json'}
+  })
+  .then((response)=>response.json())
+  .then((data)=>{
+    console.log(data.result)
+    if (data.result === "ok"){
+      setTasks([])
     }
-  });
-
-	// eliminar item en array
-	const deleteItem = (id) => {
-		setData(data.filter((item, index) => {
-		  return index != id;
-		}))
-	  }
-
-	  //el array
-	  const listItems = data.map((item, index) =>
-  <li>{item} <button onClick={() => remove(index)}>
-  </button>X</li>
-  );
-
-//para hacer el fetch
-useEffect (()=>{
-  crearUsuario();
-  obtenerTodoList();
-  update();
-},[])
-
-
-	return (
-		<>
-<form className="container" onSubmit={enviarPendientes}>
-  <div className="mb-3">
-    <label htmlFor="exampleInputEmail1" className="form-label">to do's</label>
-    {/*2. definimos el evento ochange en el input */}
-	<input type="text" className="form-control" onChange={handlePendientes} value={pendientes}/>
-  </div>
-  <button type="submit" onClick={enviarPendientes} className="btn btn-primary">Submit</button>
-  <div id="contenedorData" className="text-light">
-                        {
-                        data.map((item, index) => (
-                            <div className="row d-flex m-2"  style={{borderRadius:"20px",backgroundColor: "rgb(182, 0, 0)"}}>
-                                <div className="col-6">
-                                    <h5 className="m-2">{item}</h5>
-                                </div>
-                                <div className="col-6 text-end">
-                                    <i className="fas fa-trash-alt align-items-end m-2 pt-1"
-                                        onClick={
-                                            () => setData(data.filter((elementoDiv, currentIndex) => index != currentIndex))
-                                    }></i>
-                                </div>
-                            </div>
-                        ))
-                    } </div>
+  })
   
-</form>
-<div>
-	
-</div>
+}
 
 
-</>
-	);
+  return (
+    <>
+      <form className="container" onSubmit={addTask}>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            To do's
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            onChange={handleTaskInput}
+            value={taskInput}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+        <div id="contenedorData" className="text-light">
+          {tasks.map((item, index) => (
+            <div
+              className="row d-flex m-2"
+              style={{ borderRadius: "20px", backgroundColor: "rgb(182, 0, 0)" }}
+            >
+              <div className="col-6">
+                <h5 className="m-2">{item}</h5>
+              </div>
+              <div className="col-6 text-end">
+                <i
+                  className="fas fa-trash-alt align-items-end m-2 pt-1"
+                  onClick={() =>
+                    setTasks(tasks.filter((element, currentIndex) => index !== currentIndex))
+                  }
+                ></i>
+              </div>
+            </div>
+          ))}
+        </div>
+      </form>
+      <div></div>
+    </>
+  );
 };
 
 export default Home;
